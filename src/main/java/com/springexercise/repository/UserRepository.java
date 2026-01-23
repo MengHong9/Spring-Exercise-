1b2cf88c6c566ea27a9ef69138cd35da5c767013
Mr.Frog <Bunmenghong7@gmail.com>
2026-01-23 09:15:54 +0700

    feat: create method to validate email and name

--- src/main/java/com/springexercise/repository/UserRepository.java
+++ src/main/java/com/springexercise/repository/UserRepository.java
@@ -3,5 +3,10 @@ package com.springexercise.repository;
 import com.springexercise.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Boolean existsByName(String name);
+    Boolean existsByEmail(String email);
+
 }
