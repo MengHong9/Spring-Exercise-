b5062252cc6d8480f4d7c9c7754aac00410113e9
Mr.Frog <Bunmenghong7@gmail.com>
2026-01-23 09:11:37 +0700

    feat: add user controller

---
+++ src/main/java/com/springexercise/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.springexercise.controller;
+
+import com.springexercise.common.response.Response;
+import com.springexercise.dto.user.UserDto;
+import com.springexercise.entity.User;
+import com.springexercise.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping
+    public ResponseEntity<Response> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Response> createUser(@Valid @RequestBody UserDto dto) {
+        return userService.addUser(dto);
+    }
+
+
+    @PutMapping("{id}")
+    public ResponseEntity<Response> updateUser(@PathVariable("id") Long id, @Valid @RequestBody UserDto dto) {
+        return userService.updateUser(id, dto);
+    }
+
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Response> deleteUser(@PathVariable("id") Long id) {
+        return userService.deleteUser(id);
+    }
+}
