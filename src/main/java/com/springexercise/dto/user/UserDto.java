44d0152e7a82d28e0f399264ca06a9296805b201
Mr.Frog <Bunmenghong7@gmail.com>
2026-01-23 09:10:53 +0700

    feat: add user dto

---
+++ src/main/java/com/springexercise/dto/user/UserDto.java
@@ -0,0 +1,81 @@
+package com.springexercise.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+
+public class UserDto {
+    private Long id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @NotNull(message = "name is required")
+    @NotBlank(message = "name can not be empty")
+    @Size(min = 4 , max = 15 , message = "name at least 4 to 15 digits")
+    private String name;
+
+
+    private int age;
+
+    @Email(message = "email must end with @gmail.com")
+    @NotNull(message = "email is required")
+    @NotBlank(message = "email can not be empty")
+    @Size(min = 10 , max = 25 , message = "email at least 10 to 25 digits")
+    private String email;
+
+    @NotNull(message = "password is required")
+    @NotBlank(message = "password can not be empty")
+    @Size(min = 5 , max = 10 , message = "password at least 5 to 10 digits")
+    private String password;
+
+
+    private String role;
+}
---
+++ src/main/java/com/springexercise/dto/user/UserResponseDto.java
@@ -0,0 +1,60 @@
+package com.springexercise.dto.user;
+
+
+public class UserResponseDto {
+    private Long id;
+    private String name;
+    private int age;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    private String password;
+    private String role;
+}
