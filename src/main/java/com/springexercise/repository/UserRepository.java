12dcd46c9aa71ee3540cabf15819f8c2161f4039
Mr.Frog <Bunmenghong7@gmail.com>
2026-01-25 00:27:00 +0700

    feat: create method find by name

--- src/main/java/com/springexercise/repository/UserRepository.java
+++ src/main/java/com/springexercise/repository/UserRepository.java
@@ -3,10 +3,13 @@ package com.springexercise.repository;
 import com.springexercise.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
+
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByName(String name);
     Boolean existsByEmail(String email);
 
+
+    List<User> findByNameContainingIgnoreCase(String name);
 }
