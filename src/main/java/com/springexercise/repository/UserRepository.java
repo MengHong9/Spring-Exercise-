1d6598868986239dd91e3fcc501d26e849741ad4
Mr.Frog <Bunmenghong7@gmail.com>
2026-01-27 01:13:49 +0700

    modify(user): change type Boolean to boolean

--- src/main/java/com/springexercise/repository/UserRepository.java
+++ src/main/java/com/springexercise/repository/UserRepository.java
@@ -7,8 +7,8 @@ import java.util.List;
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Boolean existsByName(String name);
-    Boolean existsByEmail(String email);
+    boolean existsByName(String name);
+    boolean existsByEmail(String email);
 
 
     List<User> findByNameContainingIgnoreCase(String name);
